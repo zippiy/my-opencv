116c550aa9c404eb8c3f1dfbd50d932e9d641e56
zippiy <3387957+zippiy@users.noreply.github.com>
2020-06-26 17:12:36 +0200

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,11 @@
 conda create --name myenv
 conda activate myenv
+conda env remove -n deepfake-detection
+
+conda env create -f environment.yml
+conda activate deepfake-detection
+conda deactivate
+
 conda install -c conda-forge opencv=4.3.0
+
+jupyter notebook
